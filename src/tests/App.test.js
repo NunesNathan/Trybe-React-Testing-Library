2e52bd72b14812c6f5e8a444050af591e5b31775
NunesNathan <nunesxd7@hotmail.com>
2021-11-30 17:58:55 -0300

    :white_check_mark: C1 - Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,55 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
+
+describe('01 - <App.js />', () => {
+  it('Teste se o topo da aplicação contém um conjunto de links de navegação.', () => {
+    renderWithRouter(<App />);
+
+    const home = screen.getByRole('link', {
+      name: /home/i });
+    expect(home).toBeInTheDocument();
+
+    const about = screen.getByRole('link', {
+      name: /about/i });
+    expect(about).toBeInTheDocument();
+
+    const favorite = screen.getByRole('link', {
+      name: /favorite/i });
+    expect(favorite).toBeInTheDocument();
+  });
+
+  it('É redirecionada para a página inicial, o clicar no link Home',
+    () => {
+      const { history } = renderWithRouter(<App />);
+
+      history.push('/');
+
+      const heading = screen.getByRole('heading', {
+        name: /encountered pokémons/i });
+      expect(heading).toBeInTheDocument();
+    });
+
+  it('É redirecionada à About clicando em About',
+    () => {
+      const { history } = renderWithRouter(<App />);
+
+      history.push('/about');
+
+      const heading = screen.getByRole('heading', {
+        name: /about/i });
+      expect(heading).toBeInTheDocument();
+    });
+
+  it('É redirecionada à Favorite Pokémons clicando em Pokémons Favoritados',
+    () => {
+      const { history } = renderWithRouter(<App />);
+
+      history.push('/favorites');
+
+      const heading = screen.getByRole('heading', {
+        name: /Favorite pokémons/i });
+      expect(heading).toBeInTheDocument();
+    });
+});
