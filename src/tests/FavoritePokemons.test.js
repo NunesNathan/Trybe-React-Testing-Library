49533c3d530027756fc87b3943b900824da701e0
NunesNathan <nunesxd7@hotmail.com>
2021-12-01 15:33:06 -0300

    :white_check_mark: C3 - Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import { FavoritePokemons } from '../components';
+import PikachuObj from '../helpers/PikachuObj';
+
+describe('03 - <FavoritePokemons.js />', () => {
+  it('É exibido a mensagem se a pessoa não tiver pokémons favoritos.', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const hasFavorite = screen.getByText(/no favorite/i);
+
+    expect(hasFavorite).toBeDefined();
+  });
+
+  it('É exibido todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [PikachuObj] } />);
+
+    const pikachu = screen.getByText(/pikachu/i);
+
+    expect(pikachu).toBeInTheDocument();
+  });
+});
